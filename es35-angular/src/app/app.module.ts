083934b9802dfa70324c752b8533e9be162600e3
Ivo Dzhakov <412757@student.fontys.nl>
2019-09-11 14:12:35 +0300

    accepted bootstrap

--- es35-angular/src/app/app.module.ts
+++ es35-angular/src/app/app.module.ts
@@ -2,20 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { DepartmentsComponent } from './departments/departments.component';
-=======
 import { TasksComponent } from './tasks/tasks.component';
->>>>>>> origin/dev
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    DepartmentsComponent
-=======
+    DepartmentsComponent,
     TasksComponent
->>>>>>> origin/dev
   ],
   imports: [
     BrowserModule
