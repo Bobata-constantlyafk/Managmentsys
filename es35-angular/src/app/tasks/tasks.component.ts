d299dcacdc1ccd0a53cba2d2793800bb4a18b019
Dimitar Dimitrov <402384@student.fontys.nl>
2019-09-15 12:02:41 +0200

    some options added like, add, delete, view details [not quite done]

--- es35-angular/src/app/tasks/tasks.component.ts
+++ es35-angular/src/app/tasks/tasks.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from './tasks.model';
-import {$} from 'protractor';
 
 @Component({
   selector: 'app-tasks',
@@ -9,21 +8,21 @@ import {$} from 'protractor';
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = [
-    {id: 1, name: 'Task1'},
-    {id: 2, name: 'Task2'},
-    {id: 3, name: 'Task3'},
-    {id: 4, name: 'Task4'},
+    new Task(1, 'Task1'),
+    new Task(2, 'Task2'),
+    new Task(3, 'Task3'),
+    new Task(4, 'Task4'),
   ];
+
+  show = false;
+
+  currentTask: Task = new Task(0, '');
 
   constructor() {}
 
   ngOnInit() {}
 
   removeTask(event, id) {
-    const el = event.target;
-    const parent = el.parentNode;
-    // parent.classList.remove('animated');
-    // parent.classList.add('animated');
     this.tasks.splice(id, 1);
   }
 
@@ -32,5 +31,14 @@ export class TasksComponent implements OnInit {
     const id = Math.max.apply(Math, this.tasks.map((task) => task.id));
     this.tasks.push(new Task(id, name));
     input.value = '';
+  }
+
+  viewDetails(event, i) {
+    this.show = true;
+    this.currentTask = this.tasks[i];
+  }
+
+  off() {
+    this.show = false;
   }
 }
