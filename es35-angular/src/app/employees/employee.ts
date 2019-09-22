0002103b1042d0f4bbe3160f27dff42c767ef868
Dechev,Boyan B.K <b.dechev@student.fontys.nl>
2019-09-22 16:24:45 +0200

    Finished week 3 assignment

--- es35-angular/src/app/employees/employee.ts
+++ es35-angular/src/app/employees/employee.ts
@@ -1,14 +1,14 @@
 export class Employee {
+id: number;
+name: string;
+lastname: string
+isEditing: boolean;
 
-    id: number;
-    name: string;
-    lastname: string
-    isEditing: boolean;
-
-    constructor(id: number, name: string,lastname: string) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
+constructor(id: number, name: string,lastname: string) {
+     this.id = id;
+    this.name = name;
+    this.lastname = lastname;
+    this.isEditing = false;
     }
     
     toggleEdit() {
