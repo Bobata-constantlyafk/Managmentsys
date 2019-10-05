4bbba1522efd0afcfbc7dd6da26c91b89657ee6c
Ivo Dzhakov <412757@student.fontys.nl>
2019-10-05 15:46:45 +0300

    commit

--- es35-angular/src/app/app.module.ts
+++ es35-angular/src/app/app.module.ts
@@ -12,13 +12,10 @@ import {EmployeesComponent} from './employees/employees.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {TaskDetailsComponent} from './tasks/task-details/task-details.component';
 import {TaskAddComponent} from './tasks/task-add/task-add.component';
-<<<<<<< HEAD
-import { DepartmentAddComponent } from './departments/department-add/department-add.component';
-import { DepartmentDetailsComponent } from './departments/department-details/department-details.component';
-=======
+import {DepartmentAddComponent} from './departments/department-add/department-add.component';
+import {DepartmentDetailsComponent} from './departments/department-details/department-details.component';
 import {RoleDetailsComponent} from './roles/role-details/role-details.component';
 import {RoleAddComponent} from './roles/role-add/role-add.component';
->>>>>>> master
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
@@ -40,13 +37,10 @@ const appRoutes: Routes = [
     DashboardComponent,
     TaskDetailsComponent,
     TaskAddComponent,
-<<<<<<< HEAD
     DepartmentAddComponent,
     DepartmentDetailsComponent,
-=======
     RoleDetailsComponent,
     RoleAddComponent,
->>>>>>> master
   ],
   imports: [
     RouterModule.forRoot(
